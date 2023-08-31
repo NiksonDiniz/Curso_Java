23330998cd44e4c8190a384b52db82a77320b46b
Nikson Diniz <nikson.diniz@gmail.com>
2023-08-31 16:52:01 -0300

    adiciona atividade retangulo seção 8

---
+++ src/section8/exercicios/atividadefuncionario/Employee.java
@@ -0,0 +1,53 @@
+package section8.exercicios.atividadefuncionario;
+
+public class Employee {
+    private String name;
+    private Double grossSalary;
+    private Double tax;
+
+    public Employee() {
+    }
+
+    public Employee(String name, Double grossSalary, Double tax) {
+        this.name = name;
+        this.grossSalary = grossSalary;
+        this.tax = tax;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getGrossSalary() {
+        return grossSalary;
+    }
+
+    public void setGrossSalary(Double grossSalary) {
+        this.grossSalary = grossSalary;
+    }
+
+    public Double getTax() {
+        return tax;
+    }
+
+    public void setTax(Double tax) {
+        this.tax = tax;
+    }
+
+    public double netSalary() {
+        return getGrossSalary() - getTax();
+    }
+
+    public void increaseSalary(double percentage) {
+        grossSalary += grossSalary * percentage / 100.0;
+    }
+
+    @Override
+    public String toString() {
+        return getName() + ", $ " + String.format("%.2f", netSalary());
+    }
+}
