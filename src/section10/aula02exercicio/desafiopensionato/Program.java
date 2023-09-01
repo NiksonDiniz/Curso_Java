83e43c4ff915cca7d8b272199363812a372b1788
Nikson Diniz <nikson.diniz@gmail.com>
2023-09-01 15:40:41 -0300

    adiciona desafio pensionato de vetores da seção 10

---
+++ src/section10/aula02exercicio/desafiopensionato/Program.java
@@ -0,0 +1,37 @@
+package section10.aula02exercicio.desafiopensionato;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Rent[] rents = new Rent[10];
+
+        System.out.print("How many rooms will be rented? ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Rent #" + i + ":");
+            System.out.print("Name: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Email: ");
+            String email = sc.next();
+            System.out.print("Room: ");
+            int room = sc.nextInt();
+
+            rents[room] = new Rent(name, email);
+        }
+
+        System.out.println("Busy rooms: ");
+        for (int i = 0; i < rents.length; i++) {
+            if (rents[i] != null) {
+                System.out.println(i + ": " + rents[i]);
+            }
+        }
+        sc.close();
+    }
+}
