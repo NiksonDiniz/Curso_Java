5da1ca9c9e3f0fbf6863bef274f16fa4caad3cbf
Nikson Diniz <nikson.diniz@gmail.com>
2023-09-01 15:00:51 -0300

    adiciona aula 2 de vetores da seção 10

--- src/section10/aula01vetoresp2/Program.java
+++ src/section10/aula01vetoresp2/Program.java
@@ -8,7 +8,29 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Digite quantos produtos: ");
+        int n = sc.nextInt();
 
+        Product[] products = new Product[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Nome do produto: ");
+            sc.nextLine();
+            String name = sc.nextLine();
+            System.out.print("Preço do produto: ");
+            double price = sc.nextDouble();
+
+            products[i] = new Product(name, price);
+        }
+
+        double sum = 0.0;
+        for (int i = 0; i < n; i++) {
+            sum += products[i].getPrice();
+        }
+
+        double avg = sum / products.length;
+
+        System.out.printf("AVERAGE PRICE = %.2f%n", avg);
 
         sc.close();
     }
