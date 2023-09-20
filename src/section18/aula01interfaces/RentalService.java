bf1011aa89ff7bef9cb7dcf5fe7d659686e60a06
Nikson Diniz <nikson.diniz@gmail.com>
2023-09-20 20:48:08 -0300

    adiciona aula 1 de interfaces seção 18

---
+++ src/section18/aula01interfaces/RentalService.java
@@ -0,0 +1,18 @@
+package section18.aula01interfaces;
+
+public class RentalService {
+    private Double pricePerDay;
+    private Double pricePerHour;
+
+    private BrazilTaxService taxService;
+
+    public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+        this.pricePerDay = pricePerDay;
+        this.pricePerHour = pricePerHour;
+        this.taxService = taxService;
+    }
+
+    public void processInvoice( CarRental carRental){
+        carRental.setInvoice(new Invoice(50.0, 10.0));
+    }
+}
